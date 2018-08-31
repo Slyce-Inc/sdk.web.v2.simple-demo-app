150b0418ef07b916b54e263de922bea0bd88b2f4
Serghei Ivanov <serghei.ivanov@slyce.it>
2018-08-31 14:41:29 -0400

    use slycedemo account, use camera capture on input

--- script.js
+++ script.js
@@ -10,26 +10,32 @@
     rivets.bind($('#results'), viewModel);
 
     
-    const slyce = new slyceSDK('staging');
-    slyce.initSlyceSpace('', '', '');
+    const slyce = new slyceSDK('production');
+    slyce.initSlyceSpace('slycedemo', 'L9VT1RPiWGBFgHEAH7hgFGeDnZgdBPWL5aCT3zH0JZ0', 'DiiCp2Y2hLyEwggtX4km84');
     
-    const imageInput = document.querySelector('#image-source-input');
-    imageInput.addEventListener('change', (e) => {
-        const imageFile = e.srcElement.files[0];
-        
-        slyce.executeWorkflow(imageFile, '', {
-            onTaskUpdated: (message) => {
-                console.log('message: ', message);
-            },
-            onTaskCompleted: (message, errors) => {
-                console.log('items: ', message.results[0].items)
-                console.log('errors: ', errors);
+    const imageInputs = document.querySelectorAll('.image-source-input');
+    console.log(imageInputs)
+    imageInputs.forEach((imageInput) => {
+        imageInput.addEventListener('change', (e) => {
+            const imageFile = e.srcElement.files[0];
+            
+            slyce.executeWorkflow(imageFile, '8qHmYrvaaVyUyWQMifN2o8', {
+                onTaskUpdated: (message) => {
+                    console.log('message: ', message);
+                },
+                onTaskCompleted: (message, errors) => {
+                    viewModel.showResults = true;
 
-                viewModel.showResults = true;
-                viewModel.errors = errors;
-                viewModel.results = message.results[0].items;
-            }
-        }, true); 
+                    viewModel.errors = errors;
+                    console.log('errors: ', errors);
+
+                    if (message && message.results) {
+                        console.log('items: ', message.results[0].items);
+                        viewModel.results = message.results[0].items;
+                    }
+                }
+            }, true); 
+        });
     });
 })()
 
